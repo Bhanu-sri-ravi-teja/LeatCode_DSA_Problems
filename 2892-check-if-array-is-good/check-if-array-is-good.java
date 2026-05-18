df2b27c0f59e68322a5d71bb6ab39115766cd7cf
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-05-18 13:25:32 +0530

    Time: 6 ms (13.07%) | Memory: 45.4 MB (8.27%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,17 +1,29 @@
 class Solution {
     public boolean isGood(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-        int temp = 1;
-        for(int i=0; i<n-1; i++){
-            if(nums[i] != temp){
-                return false;
-            }
-            temp++;
-        }
-        if(nums[n-1] != --temp){
+        int max = nums[nums.length - 1];
+        if (nums.length != (max + 1)) {
             return false;
         }
-        return true;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            else {
+                map.put(nums[i], 1);
+            }
+        }
+        List<Integer> list = new ArrayList<>(map.values());
+        int count = 0;
+        for (int num : list) {
+            if (num >= 2) {
+                count++;
+            }
+        }
+        if (map.get(max) == 2 && (count == 1)) {
+            return true;
+        }
+        return false;
     }
 }
