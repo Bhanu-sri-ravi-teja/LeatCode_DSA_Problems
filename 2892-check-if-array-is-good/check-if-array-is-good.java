679c62ea16dc37676832b06d5732a8e2974488a5
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-05-18 13:29:14 +0530

    Time: 5 ms (37.94%) | Memory: 45.3 MB (15.19%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -3,7 +3,10 @@ class Solution {
         
         
         Arrays.sort(nums);
-        
+        int max = nums[nums.length - 1];
+        if (nums.length != (max + 1)) {
+            return false;
+        }
         int temp = 1;
         for(int i=0; i<nums.length-1; i++){
             if(nums[i] != temp){
